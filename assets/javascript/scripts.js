e5ca2f5cefb7c99d6a5fa162897efd13e2ce344b
chris green <greench@gmail.com>
2018-10-12 19:20:39 -0700

    crystal updates

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-  // Basic Global Variable info
+  // Basic Global Variables
   var wins = 0;
   var loss = 0;
   var userCrystal = 0;
   var randomCrystals = [];
-  var randomNumber; 
+  var randomNumber = [];
 
   // reset function
   function reset() {
@@ -13,34 +13,36 @@ $(document).ready(function () {
     $("#uservalue").text("0");
     randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     $("#target").text(randomNumber);
-  
+    
+
+    // end reset function
   };
 
 
-// win condition
-function checkWinOrLose() {
-  if (randomNumber === userCrystal) {
-    wins++;
-    $("#wins").text(wins);
-    $('#winCounter').text("CONGRATULATIONS YOU GOT IT!");
-    reset();
-  }
-  // If usercrystal value goes over the random number they lose, loss +1 
-  else if (randomNumber < userCrystal) {
-    loss++;
-    $("#losses").text(loss);
-    $('#winCounter').text("YOU BUST");
-    reset();
-}
-} // end checkwinorlose function
+  // win condition
+  function checkWinOrLose() {
+    if (randomNumber === userCrystal) {
+      wins++;
+      $("#wins").text(wins);
+      $('#winCounter').text("CONGRATULATIONS YOU GOT IT!");
+      reset();
+    }
+    // If usercrystal value goes over the random number they lose, loss +1 
+    else if (randomNumber < userCrystal) {
+      loss++;
+      $("#losses").text(loss);
+      $('#winCounter').text("YOU BUST");
+      reset();
+    }
+  } // end checkwinorlose function
 
 
- // generate a random number between 19 and 120 - place it in a the html
-randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
- $("#target").text(randomNumber);
+  // generate a random number between 19 and 120 - place it in a the html
+  randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  $("#target").text(randomNumber);
 
 
- // genereate 4 random numbers for the 4 crystals
+  // genereate 4 random numbers for the 4 crystals
   // every crystal has a random num 1 - 12, no doubles - at least 1 odd number - tbd ?
   while (randomCrystals.length < 4) {
     var crystalNumbers = Math.floor(Math.random() * 12) + 1;
@@ -50,29 +52,44 @@ randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     console.log(randomCrystals);
   }
 
-  // click events for the html crystal elements60%
+
+
+
+
+
+
+
+
+  // click events for the html crystal elements
   $("#sky").on("click", function () {
+    $('#winCounter').text("");
     userCrystal += randomCrystals[0];
     $("#uservalue").text(userCrystal);
     checkWinOrLose();
-    
+
   });
 
+
   $("#red").on("click", function () {
+    $('#winCounter').text("");
     userCrystal += randomCrystals[1];
     $("#uservalue").text(userCrystal);
     checkWinOrLose();
 
   });
 
-  $("#blue").on("click", function () {
+
+  $("#yellow").on("click", function () {
+    $('#winCounter').text("");
     userCrystal += randomCrystals[2];
     $("#uservalue").text(userCrystal);
     checkWinOrLose();
 
   });
 
+
   $("#green").on("click", function () {
+    $('#winCounter').text("");
     userCrystal += randomCrystals[3];
     $("#uservalue").text(userCrystal);
     checkWinOrLose();
@@ -82,9 +99,25 @@ randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
 
 
-  
 
   // end document ready function
 });
 
 
+
+
+// another way to get 4 random numbers
+
+// for (var i = 0; randomCrystals.length < 4; i++) {
+//   crystalNumbers = Math.floor(Math.random() * 12)+1;
+
+//   if (randomCrystals.includes(crystalNumbers) && randomCrystals.length < 4) {
+//     crystalNumbers = Math.floor(Math.random() * 12)+1;
+//       // console.log(random);
+//       continue;
+//   } 
+//   else {
+//     randomCrystals.push(crystalNumbers); 
+//   }
+//   console.log(randomCrystals);
+// }
