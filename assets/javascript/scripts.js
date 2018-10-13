a2f5437ff5bba4f8a714b0ea61c3f71eaaa6b5eb
chris green <greench@gmail.com>
2018-10-12 20:50:43 -0700

    updated with working 4 crystals on resets

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+// Basic Global Variables
+var wins = 0;
+var loss = 0;
+var userCrystal = 0;
+var randomCrystals = [];
+var randomNumber = [];
 
-  // Basic Global Variables
-  var wins = 0;
-  var loss = 0;
-  var userCrystal = 0;
-  var randomCrystals = [];
-  var randomNumber = [];
+// set the document to ready
+$(document).ready(function () {
+  reset();
 
   // reset function
   function reset() {
     userCrystal = 0;
-    $("#uservalue").text("0");
     randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#uservalue").text(userCrystal);
     $("#target").text(randomNumber);
-    
-
+    rCrystals()
     // end reset function
   };
 
-
-  // win condition
+  // check win / lose  condition
   function checkWinOrLose() {
     if (randomNumber === userCrystal) {
       wins++;
@@ -37,30 +37,25 @@ $(document).ready(function () {
   } // end checkwinorlose function
 
 
-  // generate a random number between 19 and 120 - place it in a the html
+  // Target Score - generate a random number between 19 and 120 - place it in a the html
   randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
   $("#target").text(randomNumber);
 
 
-  // genereate 4 random numbers for the 4 crystals
+  // Crystal Values - genereate 4 random numbers for the 4 crystals
   // every crystal has a random num 1 - 12, no doubles - at least 1 odd number - tbd ?
-  while (randomCrystals.length < 4) {
-    var crystalNumbers = Math.floor(Math.random() * 12) + 1;
-    if (randomCrystals.indexOf(crystalNumbers) > -1) continue;
-    randomCrystals[randomCrystals.length] = crystalNumbers;
-    // lets you see the 4 random numbers in the console log
-    console.log(randomCrystals);
+  function rCrystals() {
+    randomCrystals = []
+    while (randomCrystals.length < 4) {
+      var crystalNumbers = Math.ceil(Math.random() * 12)
+      if (randomCrystals.indexOf(crystalNumbers) === -1) { randomCrystals.push(crystalNumbers) }
+    }
+    console.log([...randomCrystals]);
   }
 
 
 
-
-
-
-
-
-
-  // click events for the html crystal elements
+  // Click Events - for the 4 Crystals: in the html crystal image elements
   $("#sky").on("click", function () {
     $('#winCounter').text("");
     userCrystal += randomCrystals[0];
@@ -99,25 +94,8 @@ $(document).ready(function () {
 
 
 
-
   // end document ready function
 });
 
 
 
-
-// another way to get 4 random numbers
-
-// for (var i = 0; randomCrystals.length < 4; i++) {
-//   crystalNumbers = Math.floor(Math.random() * 12)+1;
-
-//   if (randomCrystals.includes(crystalNumbers) && randomCrystals.length < 4) {
-//     crystalNumbers = Math.floor(Math.random() * 12)+1;
-//       // console.log(random);
-//       continue;
-//   } 
-//   else {
-//     randomCrystals.push(crystalNumbers); 
-//   }
-//   console.log(randomCrystals);
-// }
